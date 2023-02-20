bb02fa18a36c020a12b5543040ec9cf7033bf073
serapieTuyishime <tuyiserapie1@gmail.com>
2023-02-20 16:53:10 +0200

    Added a markdown

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,62 +1,39 @@
-import { React } from "react";
-export default function Sidebar({
-    createNote,
-    notes,
-    setNotes,
-    activeNote,
-    activateNote,
-    setNotesCount,
-    notesCount,
-}) {
-    return (
-        <div className="w-1/3">
-            <label
-                className="flex justify-between w-full text-2xl cursor-pointer"
-                onClick={() => {
-                    createNote(`Note ${notesCount}`);
-                    setNotesCount(notesCount + 1);
-                }}
-            >
-                <span>New Note</span>
-                <span className="flex items-center justify-center w-8 h-8 text-white bg-violet-400">
-                    +
-                </span>
-            </label>
-            <div className="grid gap-3 p-2">
-                {notes.length !== 0 ? (
-                    notes.map((note, index) => {
-                        let noteObject = JSON.parse(note);
-                        return (
-                            <label
-                                className={
-                                    activeNote === noteObject.id
-                                        ? "text-white bg-gray-700"
-                                        : ""
-                                }
-                                key={index}
-                                onClick={() => activateNote(noteObject.id)}
-                            >
-                                {noteObject.name.substring(0, 15)}
-                            </label>
-                        );
-                    })
-                ) : (
-                    <>No notes</>
-                )}
+import React from "react";
 
-                {notes.length !== 0 && (
-                    <label
-                        className="cursor-pointer"
-                        onClick={() => {
-                            localStorage.setItem("notes", "");
-                            setNotes([]);
-                            setNotesCount(0);
-                        }}
-                    >
-                        Clear notes
-                    </label>
-                )}
+export default function Sidebar(props) {
+    const noteElements = props.notes.map((note, index) => (
+        <div key={note.id} className="w-full">
+            <div
+                className={`title ${
+                    note.id === props.currentNote.id ? "bg-[#4a4e74]" : ""
+                }`}
+                onClick={() => props.setCurrentNoteId(note.id)}
+            >
+                <h4 className="text-primary">
+                    {note.body.split("\n")[0].substring(0, 21)}
+                </h4>
+                <button
+                    className="delete-btn"
+                    onClick={(event) => props.deleteNote(event, note.id)}
+                >
+                    <i className="gg-trash trash-icon"></i>
+                </button>
             </div>
         </div>
+    ));
+
+    return (
+        <section className="w-1/5 h-full overflow-y-auto">
+            <div className="sidebar--header">
+                <h3>Notes</h3>
+                <button
+                    className="outline-none cursor-pointer bg-[#4a4e74] border-none text-white rounded-md h-8 w-8"
+                    onClick={props.newNote}
+                >
+                    +
+                </button>
+            </div>
+            {noteElements}
+        </section>
     );
 }
